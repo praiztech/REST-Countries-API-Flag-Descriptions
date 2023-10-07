1193eb0c8cac260d14a014639b48d444aa1b6c00
praiztech <praisefagbabiaka@gmail.com>
2023-10-07 12:57:29 +0100

    Created a folder for my flag description contribution

---
+++ README.md
@@ -0,0 +1,11 @@
+# Rest Countries API Flag Descriptions
+
+This repository contains the files used to add flag descriptions to the REST Countries API.
+
+These include:
+
+- The flag descriptions json file
+- The initial python script used to add a flag description field to the API
+- The final python script used to modify the API's flag field to include an alt field based on feedback from the API maintainer
+
+The [REST Countries API repository](https://gitlab.com/restcountries/restcountries) is hosted on GitLab.
---
+++ add_alt_to_flag.py
@@ -0,0 +1,40 @@
+import json
+
+country_data = []
+#sovereigns = []
+#sovereigns_count = 0
+try:
+    flag_fh = open('flag-descriptions.json')
+    flag_descriptions = json.load(flag_fh)
+    with open('src/main/resources/countriesV3.1.json') as fh:
+        data = json.load(fh)
+        for country_count, country in enumerate(data):
+            flag_data = None
+			# Query country's common name.
+		    # For countries that share common names e.g. Guinea,
+			# query country's official name
+            if country['name']['common'] in flag_descriptions:
+                flag_country_name = country['name']['common']
+            elif country['name']['official'] in flag_descriptions:
+                flag_country_name = country['name']['official']
+            try:
+                if country['independent'] == True:
+                    #sovereigns.append(country['name']['common'])
+                    #sovereigns_count += 1
+                    flag_data = {'svg': country['flags']['flagImages']['svg'],
+                                'png': country['flags']['flagImages']['png'],
+                                'alt': flag_descriptions[flag_country_name]}
+            except KeyError:
+			# Don't alter data of non-independent countries
+                pass
+            finally:
+                country_data.append(country)
+                if flag_data is not None:
+                    country_data[country_count]['flags'] = flag_data
+finally:
+    flag_fh.close()
+with open('src/main/resources/countriesV3.1.json', 'w', encoding='utf-8') as write_fh:
+    json.dump(country_data, write_fh, indent=2, separators=(", ", ": "))
+#print(len(country_data))
+#print(sovereigns_count)
+#print(sorted(sovereigns))
